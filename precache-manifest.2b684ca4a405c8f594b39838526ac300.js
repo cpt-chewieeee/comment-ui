0670b94e8a522f5af33987e81b9a8f3a8a8958d4
hongyi zhang <hzhan014@gmail.com>
2018-12-16 11:53:26 -0800

    Updates

---
+++ precache-manifest.2b684ca4a405c8f594b39838526ac300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "90bf451e3e309bed7c17",
+    "url": "/static/js/main.90bf451e.chunk.js"
+  },
+  {
+    "revision": "d4e09657c70030c0c690",
+    "url": "/static/js/1.d4e09657.chunk.js"
+  },
+  {
+    "revision": "90bf451e3e309bed7c17",
+    "url": "/static/css/main.79e4ffd8.chunk.css"
+  },
+  {
+    "revision": "2909deaeaf886a10d97234fe4ea31d3d",
+    "url": "/index.html"
+  }
+];
